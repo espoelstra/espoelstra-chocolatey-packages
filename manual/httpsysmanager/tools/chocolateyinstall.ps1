b33dd347bcd193bc6baa91a2690d25dc196311ce
Ethan Spoelstra <Ethan.Spoelstra@FCStone.com>
2016-12-30 13:32:33 -0600

    Created repo from chocolatey-packages-template for httpsysmanager

---
+++ manual/httpsysmanager/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName= 'httpsysmanager'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://httpsysmanager.codeplex.com/downloads/get/475350'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'httpsysmanager*'
+  checksum      = ''
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
